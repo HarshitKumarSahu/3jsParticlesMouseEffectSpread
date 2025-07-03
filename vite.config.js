85468e77ef62e3698c36430fdc801139f50c2f15
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-07-03 20:16:13 +0530

    final code 3

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [glsl()]
+  plugins: [glsl()],
+  resolve: {
+    alias: {
+      'three/examples/jsm': 'node_modules/three/examples/jsm',
+    },
+  },
 });
